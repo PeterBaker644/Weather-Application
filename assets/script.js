6d1f456daf867b6b1fca7d53931f28e33ef525c9
Excellion <baker644@umn.edu>
2020-05-07 19:42:16 -0400

    Finally got the geolocator plugged in and working

--- assets/script.js
+++ assets/script.js
@@ -17,48 +17,64 @@ window.onload = function () {
         addressLookup: true,    // requires Google API key if true
         timezone: true         // requires Google API key if true
     };
-    geolocator.locate(options, function (err, location) {
-        if (err) return console.log(err);
+    geolocator.locate(options, function locate(err, location) {
+        if (err) {
+            updateMain = true;
+            return console.log(err);
+        }
         console.log(location);
-        const userLocation = location;
+        userCity.city = location.address.city;
+        userCity.state = location.address.stateCode;
+        userCity.country = location.address.countryCode;
+        // will need to change with countries. Do later.
+        $("#header-info").prepend(userCity.city + ", " + userCity.state);
+        console.log("appended header");
+        $("#header-icon").addClass(getCityWeather(userCity.city));
+        $("#time").text(moment().format(" h:mma"));
+        console.log("added time");
     });
 };
 
-var currentCity = { "name": "", "country": "", "longitude": "", "latitude": "" }
+var userCity = { "city": "", "state": "", "country": ""}
+var currentCity = { "city": "", "state": "", "country": "", "longitude": "", "latitude": "" }
 var searchedCities = []
-var addCity = true;
+var addCity = false;
+var updateMain = false;
 // put this on a timer
-$("#info-header").append(moment().format(" h:mma"));
+
 
 // https://onury.io/geolocator/ <- Use this. And try to get it to work.
 
-// function geolocate() {
-//     function success(position) {
-//       const userLatitude  = position.coords.latitude;
-//       const userLongitude = position.coords.longitude;
-//     }
-//     function error() {
-//       alert('Unable to retrieve your location');
-//     } 
-//     if(!navigator.geolocation) {
-//       alert('Geolocation is not supported by your browser');
-//     } else {
-//       navigator.geolocation.getCurrentPosition(success, error);
-//     }
-// }
-
-function getCityWeather(city) {
-    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=be3bce43079bfb84af95f638fe28b8ec";
+function getCityWeather(searchTerm) {
+   
+    geolocator.geocode(searchTerm, function (err, location) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(err || location);
+        currentCity.city = location.address.city;
+        if (location.address.countryCode == "US") {
+            currentCity.state = location.address.stateCode;
+        }
+        currentCity.country = location.address.countryCode;
+        currentCity.longitude = location.coords.longitude.toFixed(4);
+        currentCity.latitude = location.coords.latitude.toFixed(4);
+    });
+             
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=be3bce43079bfb84af95f638fe28b8ec";
 
     $.ajax({
         url: cityURL,
         method: "GET",
     }).then(function (response) {
         console.log(response)
-        currentCity.name = response.name;
-        currentCity.country = response.sys.country;
-        currentCity.longitude = response.coord.lon;
-        currentCity.latitude = response.coord.lat; 
+        // if (currentCity.country == "US") {
+
+        // }
+        // currentCity.name = response.name;
+        // currentCity.country = response.sys.country;
+        // currentCity.longitude = response.coord.lon;
+        // currentCity.latitude = response.coord.lat; 
 
         weatherCode = response.weather[0].id;
         hourUnix = Number(moment.utc().format('X')); // Get current UTC
@@ -68,15 +84,25 @@ function getCityWeather(city) {
         sunset = moment(response.sys.sunset, 'X').format("H");
 
         iconClass = getIcon(Number(weatherCode), Number(hour), Number(sunrise), Number(sunset));
-
-        $("#city-name").text(response.name + ", " + response.sys.country);
-        $("#wind-speed").html(`${Math.round(response.wind.speed)}<small>mph</small>`);
-        $("#humidity").html(`${response.main.humidity}<small>%</small>`);
-        $("#temperature").text(Math.round(response.main.temp) + "°");
-        $("#i-weather-current").removeClass();
-        $("#i-weather-current").addClass(iconClass);
-
-        getForecast();
+        
+        if (updateMain) {
+            if (currentCity.country !== "US") {
+                $("#city-name").text(response.name + ", " + currentCity.country);
+            } else {
+                $("#city-name").text(response.name + ", " + currentCity.state);
+            } 
+            $("#wind-speed").html(`${Math.round(response.wind.speed)}<small>mph</small>`);
+            $("#humidity").html(`${response.main.humidity}<small>%</small>`);
+            $("#temperature").text(Math.round(response.main.temp) + "°");
+            $("#i-weather-current").removeClass();
+            $("#i-weather-current").addClass(iconClass);
+            getForecast();
+        } else {
+            console.log("added class to icon");
+            updateMain = true;
+            addCity = true;
+            return (iconClass);
+        }
         
         if (addCity) {
             saveName();
@@ -150,10 +176,15 @@ function saveName() {
     for (val in searchedCities) {
         entry = searchedCities[val];
         console.log("log entry:" + entry);
-        button = $("<button>").text(entry.name + ", " + entry.country);
+        // needs to be changed if we start using state names
+        if (entry.country !== "US") {
+            fullName = entry.city + ", " + entry.country;
+        } else {
+            fullName = entry.city + ", " + entry.state;
+        }
+        button = $("<button>").text(fullName);
+        button.attr("data-city", fullName)
         button.addClass("list-group-item list-group-item-action");
-        // This line might need to chnge in the future depending on contents
-        button.attr("data-city", entry.name);
         $("#city-list").append(button);
         if (searchedCities.length >= 9) {
             searchedCities.pop();
