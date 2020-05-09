b6abadee70f2737d8719e030186af4ad121cc02a
Blackstar <baker644@umn.edu>
2020-05-09 16:57:41 -0500

    Fixed a bugs and got storage working. Still a bit messy

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+var currentCity = { "city": "", "state": "", "country": "", "longitude": "", "latitude": "" };
+var searchedCities = [];
+var saveLocation = false;
+var writeHeader = true;
+
 geolocator.config({
     language: "en",
     google: {
@@ -7,53 +12,37 @@ geolocator.config({
 });
 
 window.onload = function () {
+    if (localStorage.getItem("history") !== null) {
+        searchedCities = JSON.parse(localStorage.getItem("history"));
+        listLocation();
+    }
     var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumWait: 10000,     // max wait time for desired accuracy
-        maximumAge: 0,          // disable cache
-        desiredAccuracy: 30,    // meters
-        fallbackToIP: true,     // fallback to IP if Geolocation fails or rejected
         addressLookup: true,    // requires Google API key if true
-        timezone: true         // requires Google API key if true
     };
-    geolocator.locate(options, function locate(err, location) {
+    geolocator.locateByIP(options, function locate(err, location) {
         if (err) {
-            updateMain = true;
             return console.log(err);
         }
-        console.log(location);
-        userCity.city = location.address.city;
-        userCity.state = location.address.stateCode;
-        userCity.country = location.address.countryCode;
-        // will need to change with countries. Do later.
-        $("#header-info").prepend(userCity.city + ", " + userCity.state);
-        console.log("appended header");
-        getCityWeather(userCity.city);
+        getCityWeather(location.address.city);
         $("#time").text(moment().format(" h:mma"));
-        console.log("added time");
     });
 };
 
- // 1
+function fullName(list) {
+    if (list.country !== "US") {
+        var fullName = list.city + ", " + list.country;
+    } else {
+        var fullName = list.city + ", " + list.state;
+    }
+    return fullName;
+}
 
-var userCity = { "city": "", "state": "", "country": ""}
-var currentCity = { "city": "", "state": "", "country": "", "longitude": "", "latitude": "" }
-var searchedCities = []
-var addCity = true;
-var updateMain = false;
-// put this on a timer
+function getCityWeather(city) {
 
-
-// https://onury.io/geolocator/ <- Use this. And try to get it to work.
-
-function getCityWeather(searchTerm) {
-   
-    geolocator.geocode(searchTerm, function (err, location) {
+    geolocator.geocode(city, function (err, location) {
         if (err) {
             return console.log(err);
         }
-        console.log(err || location);
         currentCity.city = location.address.city;
         if (location.address.countryCode == "US") {
             currentCity.state = location.address.stateCode;
@@ -61,56 +50,41 @@ function getCityWeather(searchTerm) {
         currentCity.country = location.address.countryCode;
         currentCity.longitude = location.coords.longitude.toFixed(4);
         currentCity.latitude = location.coords.latitude.toFixed(4);
-    });
-             
-    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=be3bce43079bfb84af95f638fe28b8ec";
 
-    $.ajax({
-        url: cityURL,
-        method: "GET",
-    }).then(function (response) {
-        console.log(response)
-        // if (currentCity.country == "US") {
+        var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity.city + "&units=imperial&appid=be3bce43079bfb84af95f638fe28b8ec";
 
-        // }
-        // currentCity.name = response.name;
-        // currentCity.country = response.sys.country;
-        // currentCity.longitude = response.coord.lon;
-        // currentCity.latitude = response.coord.lat; 
+        $.ajax({
+            url: cityURL,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response)
+            
+            weatherCode = response.weather[0].id;
+            hourUnix = Number(moment.utc().format('X')); // Get current UTC
+            hourOffset = Number(response.timezone); // Get UTC timezone offset
+            hour = moment.unix(hourUnix + hourOffset).utc().format("H"); // Calculate UNIX timestamp
+            sunrise = moment(response.sys.sunrise, 'X').format("H");
+            sunset = moment(response.sys.sunset, 'X').format("H");
 
-        weatherCode = response.weather[0].id;
-        hourUnix = Number(moment.utc().format('X')); // Get current UTC
-        hourOffset = Number(response.timezone); // Get UTC timezone offset
-        hour = moment.unix(hourUnix + hourOffset).utc().format("H"); // Calculate UNIX timestamp
-        sunrise = moment(response.sys.sunrise, 'X').format("H");
-        sunset = moment(response.sys.sunset, 'X').format("H");
-
-        iconClass = getIcon(Number(weatherCode), Number(hour), Number(sunrise), Number(sunset));
-        
-        if (updateMain) {
-            if (currentCity.country !== "US") {
-                $("#city-name").text(response.name + ", " + currentCity.country);
-            } else {
-                $("#city-name").text(response.name + ", " + currentCity.state);
-            } 
-            $("#wind-speed").html(`${Math.round(response.wind.speed)}<small>mph</small>`);
-            $("#humidity").html(`${response.main.humidity}<small>%</small>`);
-            $("#temperature").text(Math.round(response.main.temp) + "°");
-            $("#i-weather-current").removeClass();
-            $("#i-weather-current").addClass(iconClass);
-            getForecast();
-        } else {
-            $("#header-icon").addClass(iconClass);
-            $("#header-icon").text(" ")
-            console.log("added class to icon");
-            updateMain = true;
-        }
-        
-        if (addCity) {
-            saveName();
-        } else {
-            addCity = true;
-        }
+            iconClass = getIcon(Number(weatherCode), Number(hour), Number(sunrise), Number(sunset));
+            
+            // if (updateMain) {
+                $("#city-name").text(fullName(currentCity));
+                $("#wind-speed").html(`${Math.round(response.wind.speed)}<small>mph</small>`);
+                $("#humidity").html(`${response.main.humidity}<small>%</small>`);
+                $("#temperature").text(Math.round(response.main.temp) + "°");
+                $("#i-weather-current").removeClass();
+                $("#i-weather-current").addClass(iconClass);
+                getForecast();
+            // }
+            if (writeHeader) {
+                $("#header-info").prepend(fullName(currentCity) + " ");
+                $("#header-icon").addClass(iconClass);
+                $("#header-icon").text(" ")
+                console.log("added class to icon");
+                writeHeader = false;
+            }
+        });
     });
 }
 
@@ -153,15 +127,15 @@ function getForecast() {
             $(this).append($("<div>").append(humidityText, humidityIcon).addClass("small"));
             i++;
         })
-
+        if (saveLocation) {
+            searchedCities.unshift({...currentCity});
+            listLocation();
+            saveLocation = false;
+        }
     });
 }
 
 function getIcon(code, hour, sunrise, sunset) {
-    console.log("sunrise is " + sunrise);
-    console.log("sunset is " + sunset);
-    console.log("hour is " + hour);
-    console.log("code is " + code);
     if (hour >= sunrise && hour < sunset) {
         var iconClass = `wi wi-owm-day-${code}`;
     }
@@ -171,22 +145,14 @@ function getIcon(code, hour, sunrise, sunset) {
     return (iconClass);
 }
 
-function saveName() {
-    searchedCities.unshift({...currentCity});
+function listLocation() {
+    localStorage.setItem("history", JSON.stringify(searchedCities));
     $("#city-list").empty();
-    console.log("should be empty")
     for (val in searchedCities) {
         entry = searchedCities[val];
-        // THIS IS TROUBLE
-        console.log("log entry:" + entry);
-        // needs to be changed if we start using state names
-        if (entry.country !== "US") {
-            fullName = entry.city + ", " + entry.country;
-        } else {
-            fullName = entry.city + ", " + entry.state;
-        }
-        button = $("<button>").text(fullName);
-        button.attr("data-city", fullName)
+        console.log
+        button = $("<button>").text(fullName(entry));
+        button.attr("data-city", fullName(entry));
         button.addClass("list-group-item list-group-item-action");
         $("#city-list").append(button);
         if (searchedCities.length >= 9) {
@@ -197,13 +163,13 @@ function saveName() {
 
 $("#search-city").on("click", function (event) {
     event.preventDefault();
+    saveLocation = true;
     getCityWeather($("#city-input").val());
-    console.log("Request Attempted")
+    $("#city-input").val("");
 });
 
 $("#city-list").on("click", function(event) {
     event.preventDefault();
-    addCity = false;
     city = event.target.getAttribute("data-city");
     getCityWeather(city);
 });
